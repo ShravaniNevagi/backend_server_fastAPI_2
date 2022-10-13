5e4bc9634a682c47f6b53b8676692e68129696eb
enid_fernandes <enid_fernandes@persistent.com>
2022-10-13 11:25:32 +0530

    host changed

--- main.py
+++ main.py
@@ -201,4 +201,4 @@ async def start_client(info : test,db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host="localhost", port=8080, reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
